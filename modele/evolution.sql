53d98ea2762d70db48831a92d9852a1f1ebd6bcd
taccary <34951152+taccary@users.noreply.github.com>
2020-10-11 19:29:27 +0200

    Add files via upload

--- modele/evolution.sql
+++ modele/evolution.sql
@@ -1,8 +1,10 @@
 
 ALTER TABLE `bateau` ADD `photo` VARCHAR(100) NULL DEFAULT NULL AFTER `nom`; 
 
-UPDATE `bateau` SET `photo` = 'korAnt.jpg' WHERE `bateau`.`id` = 1; 
-UPDATE `bateau` SET `photo` = 'ArSolen.jpg' WHERE `bateau`.`id` = 2; 
-UPDATE `bateau` SET `photo` = 'alXi.jpg' WHERE `bateau`.`id` = 3; 
-UPDATE `bateau` SET `photo` = 'luceIsle.jpg' WHERE `bateau`.`id` = 4; 
-UPDATE `bateau` SET `photo` = 'maellys.jpg' WHERE `bateau`.`id` = 5; 
+UPDATE `bateau` SET `photo` = 'images/bateaux/korAnt.jpg' WHERE `bateau`.`id` = 1; 
+UPDATE `bateau` SET `photo` = 'images/bateaux/ArSolen.jpg' WHERE `bateau`.`id` = 2; 
+UPDATE `bateau` SET `photo` = 'images/bateaux/alXi.jpg' WHERE `bateau`.`id` = 3; 
+UPDATE `bateau` SET `photo` = 'images/bateaux/luceIsle.jpg' WHERE `bateau`.`id` = 4; 
+UPDATE `bateau` SET `photo` = 'images/bateaux/maellys.jpg' WHERE `bateau`.`id` = 5; 
+
+
